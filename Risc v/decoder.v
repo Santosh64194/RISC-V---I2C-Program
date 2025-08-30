ff8685c1af3616a683084d20055e7540cd6df366
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-08-30 20:52:41 +0530

    Update decoder.v

--- Risc v/decoder.v
+++ Risc v/decoder.v
@@ -3,7 +3,7 @@
 module decoder(
     input [31:0] instr,
     output  reg_write,
-    output reg [3:0] alucontrol 
+    output [3:0] alucontrol 
 );
 
 reg reg_writ;
@@ -18,17 +18,26 @@ always@(*) begin
 end
 
 always@(*) begin
-case({(isReg || isImm), instr[14:12]})
-    4'b1000 : alucontrol = (instr[30]) ? 4'h1 : 4'h0;
-    4'b1100 : alucontrol = 4'h4;
-    4'b1110 : alucontrol = 4'h3;
-    4'b1111 : alucontrol = 4'h2;
-    4'b1001 : alucontrol = 4'h5;
-    4'b1101 : alucontrol = (instr[30]) ? 4'h7 : 4'h6;
-    4'b1010 : alucontrol = 4'h9;
-    4'b1011 : alucontrol = 4'h8;
-endcase
-end
+    if(isBranch)
+        case(instr[14:12])
+            3'b000 : alucontrol = 4'h0;
+            3'b001 : alucontrol = 4'h1;
+            3'b100 : alucontrol = 4'h2;
+            3'b101 : alucontrol = 4'h3;
+            3'b110 : alucontrol = 4'h4;
+            3'b111 : alucontrol = 4'h5;
+        endcase
+    else if(isReg || isImm)
+        case({(isReg || isImm), instr[14:12]})
+            4'b1000 : alucontrol = (instr[30]) ? 4'h1 : 4'h0;
+            4'b1100 : alucontrol = 4'h4;
+            4'b1110 : alucontrol = 4'h3;
+            4'b1111 : alucontrol = 4'h2;
+            4'b1001 : alucontrol = 4'h5;
+            4'b1101 : alucontrol = (instr[30]) ? 4'h7 : 4'h6;
+            4'b1010 : alucontrol = 4'h9;
+            4'b1011 : alucontrol = 4'h8;
+        endcase
 
 assign reg_write = reg_writ;
 endmodule
