c94bb7418fcede9024869c26640c12b4ad8e7750
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-05 17:18:44 +0530

    Add files via upload

---
+++ Risc v/PC_mux.v
@@ -0,0 +1,25 @@
+`timescale 1ns/ps
+
+// module pcmux(
+//     input sel,
+//     input [31:0] four, immtarget,
+//     output [31:0] out_pc
+// );
+
+// if(!sel) assign out_pc = four;
+// else assign out_pc = immtarget;
+
+// endmodule
+
+module pcmuxd(
+    input clk,
+    input [31:0] in,
+    output [31:0] out
+);
+reg [31:0] pcout;
+
+always@(posedge clk) pcout <= in;
+
+assign out = pcout;
+
+endmodule
