15c9d579e12267176e0c2b37f09f839bfc1564ba
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-05 17:19:14 +0530

    Add files via upload

---
+++ Risc v/output_reg.v
@@ -0,0 +1,21 @@
+`timescale 1ns/ps
+
+module out_reg (
+    input [31:0] alu_result, 
+    input [31:0] data_mem_result,
+    input [31:0] PC,
+    input [1:0] sel,
+    output [31:0] result
+);
+
+    reg [31:0] re;
+    always@(*) begin
+        case(sel)
+            2'b00 : re = alu_result;
+            2'b01 : re = data_mem_result;
+            2'b10 : re = PC;
+        endcase
+    end
+    assign result = re;
+
+endmodule 
