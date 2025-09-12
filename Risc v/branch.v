bdec10862615da45ec63e85085b03d4c6b2487d6
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-12 15:08:42 +0530

    Update branch.v

--- Risc v/branch.v
+++ Risc v/branch.v
@@ -1,8 +1,3 @@
-`timescale 1ns/1ps
-
-module branch(
-    input [31:0] A,B,
-    input [3:0] control,
     output root
 );
 
@@ -10,12 +5,12 @@ reg broot;
 
 always@(*)
     case(control)
-        4'b0000 : broot = (A == B) 1 : 0;
-        4'b0001 : broot = (A != B) 1 : 0;
-        4'b0010 : broot = ($signed(A) < $signed(B)) 1 : 0;
-        4'b0011 : broot = ($signed(A) >= $signed(B)) 1 : 0;
-        4'b0100 : broot = (A < B) 1 : 0;
-        4'b0101 : broot = (A >= B) 1 : 0;  
+      4'b0000 : broot = (A == B) ? 1 : 0;
+      4'b0001 : broot = (A != B) ? 1 : 0;
+      4'b0010 : broot = ($signed(A) < $signed(B)) ? 1 : 0;
+      4'b0011 : broot = ($signed(A) >= $signed(B)) ? 1 : 0;
+      4'b0100 : broot = (A < B) ? 1 : 0;
+      4'b0101 : broot = (A >= B) ? 1 : 0;  
         default : broot = 0;     
     endcase
 
