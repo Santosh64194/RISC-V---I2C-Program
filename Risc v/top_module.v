8b2906f7e06991f418cf43484cdacfaccacbd0ee
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-24 20:49:12 +0530

    Update top_module.v

--- Risc v/top_module.v
+++ Risc v/top_module.v
@@ -15,7 +15,7 @@ module riscv_top(
     wire [31:0] reg_read_data2;
     wire [31:0] alu_input2;
     wire [31:0] alu_result;
-    wire alu_zero; 
+    wire alu_zero, write_data; 
     wire [31:0] result;
     wire [31:0] dmem_temp_rslt;
     wire [1:0] result_src;
@@ -43,7 +43,8 @@ module riscv_top(
       .ImmSrc(Immsrc),
       .is_branch_instr(is_branch_instr),
       .is_jmp_instr(isJump),
-      .is_jmpr_instr(isJumpR)
+      .is_jmpr_instr(isJumpR),
+      .wed(write_data)
     );
     regFile reg_file_inst (
         .clk(clk),
@@ -72,6 +73,7 @@ module riscv_top(
 
     out_reg outreg(
         .alu_result(alu_result),
+        .data_mem_result(dmem_temp_rslt),
         .PC(pc_plus_4),
         .sel(result_src),
         .result(result)
@@ -82,15 +84,24 @@ module riscv_top(
         .in(tempPC),
         .out(pc)
     );
+    Dmem dmem(
+        .clk(clk),
+        .we(write_data),
+        .a(mem_addr),
+        .wd(reg_read_data2),
+        .func3(instruction[14:12]),
+        .rd(dmem_temp_rslt)
+    );
 
     assign ALUSrc = (instruction[6:0] == 7'b0010011);
     assign alu_input2 = ALUSrc ? immediate : reg_read_data2;
-
+    assign mem_addr = reg_read_data1 + immediate;
 always @(*) 
     if (Immsrc || isJump) 
         case (instruction[6:0])
-            7'b0010011 : immediate = {{20{instruction[31]}}, instruction[31:20]}; // I-type
-            7'b1100111 : immediate = {{20{instruction[31]}}, instruction[31:20]}; // JALR I-type
+            // 7'b0010011 : immediate = {{20{instruction[31]}}, instruction[31:20]}; // I-type
+            // 7'b0000011 : immediate = {{20{instruction[31]}}, instruction[31:20]}; // Load Imm
+            7'b0000011 ,7'b0010011, 7'b1100111 : immediate = {{20{instruction[31]}}, instruction[31:20]}; // JALR I-type
             7'b0100011 : immediate = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; // S-type
             7'b1100011 : immediate = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // B-type
             7'b1101111 : immediate = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21],1'b0}; // J-type
