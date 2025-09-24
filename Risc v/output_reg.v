a58032c828b2211f4d4cb47bd05f32d997b7bd2b
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-24 20:50:16 +0530

    Update output_reg.v

--- Risc v/output_reg.v
+++ Risc v/output_reg.v
@@ -12,10 +12,10 @@ module out_reg (
     always@(*) begin
         case(sel)
             2'b00 : re = alu_result;
+            2'b01 : re = data_mem_result;
             2'b10 : re = PC;
         endcase
     end
     assign result = re;
 
 endmodule 
-
