c03629cadc57f28b33d971178265a8c173209d11
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-24 20:48:45 +0530

    Update instruction_memory.v

--- Risc v/instruction_memory.v
+++ Risc v/instruction_memory.v
@@ -4,13 +4,13 @@ module Imem (
 );
   reg [31:0] storage [0:10000];
 
-    initial begin
-        storage[0] = 32'h00000013; // NOP (ADDI x0, x0, 0)
-        storage[1] = 32'h00100093; // ADDI x1, x0, 1
-        storage[2] = 32'h00200113; // ADDI x2, x0, 2
-        storage[3] = 32'h002081B3; // ADD x3, x1, x2
-        storage[4] = 32'h00310023; // SW x3, 0(x2)
-    end
+    // initial begin
+    //     storage[0] = 32'h00000013; // NOP (ADDI x0, x0, 0)
+    //     storage[1] = 32'h00100093; // ADDI x1, x0, 1
+    //     storage[2] = 32'h00200113; // ADDI x2, x0, 2
+    //     storage[3] = 32'h002081B3; // ADD x3, x1, x2
+    //     storage[4] = 32'h00310023; // SW x3, 0(x2)
+    // end
 
   always @(*) rd = storage[a >> 2];
 
