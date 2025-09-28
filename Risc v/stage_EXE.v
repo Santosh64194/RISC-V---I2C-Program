57703779de426a58d6fe83fe4ec9b82853966816
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-09-28 15:41:32 +0530

    Update stage_EXE.v

--- Risc v/stage_EXE.v
+++ Risc v/stage_EXE.v
@@ -6,6 +6,9 @@ module stage2 (
     input [1:0] in_result_src,
     input [31:0] in_pc_plus_4, in_alu_result, in_read_data,
     input [4:0] in_a_wr,
+    input [2:0] in_func3,
+
+    output [2:0] o_func3,
     output reg o_RegWrite, 
     output reg o_wed,
     output reg [1:0] o_result_src,
@@ -23,6 +26,7 @@ module stage2 (
            o_alu_result <= 0;
            o_read_data <= 0;
            o_a_wr <= 0;
+           o_func3 <= 0;
         end else if (!stall) begin
            o_RegWrite <= in_RegWrite;
            o_wed <= in_wed;
@@ -31,6 +35,7 @@ module stage2 (
            o_alu_result <= in_alu_result;
            o_read_data <= in_read_data;
            o_a_wr <= in_a_wr;
+           o_func3 <= in_func3;
         end
     end
 endmodule
