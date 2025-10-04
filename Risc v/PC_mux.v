a7f15973b553363119f149b54e2654a23707c3c1
Monish-VR <119118249+Monish-VR@users.noreply.github.com>
2025-10-04 22:41:36 +0530

    Update PC_mux.v

--- Risc v/PC_mux.v
+++ Risc v/PC_mux.v
@@ -1,16 +1,5 @@
 `timescale 1ns/1ps
 
-// module pcmux(
-//     input sel,
-//     input [31:0] four, immtarget,
-//     output [31:0] out_pc
-// );
-
-// if(!sel) assign out_pc = four;
-// else assign out_pc = immtarget;
-
-// endmodule
-
 module pcmuxd(
     input clk,
     input [31:0] in,
@@ -21,6 +10,5 @@ reg [31:0] pcout;
 always@(posedge clk) pcout <= in;
 
 assign out = pcout;
-
 
 endmodule
